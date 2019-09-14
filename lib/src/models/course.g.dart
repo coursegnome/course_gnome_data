49af55d2593da4c3baaaf04e967d8ab2b8c420eb
timtraversy <timtrav@gmail.com>
2019-09-13 20:44:22 -0400

    Built gen files

--- lib/src/models/course.g.dart
+++ lib/src/models/course.g.dart
@@ -8,19 +8,20 @@ part of 'course.dart';
 
 Offering _$OfferingFromJson(Map<String, dynamic> json) {
   return Offering(
-      name: json['name'] as String,
-      deptAcr: json['deptAcr'] as String,
-      deptNum: json['deptNum'] as String,
-      credit: json['credit'] as String,
-      status: _$enumDecodeNullable(_$StatusEnumMap, json['status']),
-      id: json['id'] as String,
-      teachers: (json['teachers'] as List)?.map((e) => e as String)?.toList(),
-      section: json['section'] as String,
-      classTimes: (json['classTimes'] as List)
-          ?.map((e) =>
-              e == null ? null : ClassTime.fromJson(e as Map<String, dynamic>))
-          ?.toList(),
-      objectID: json['objectID'] as String);
+    name: json['name'] as String,
+    deptAcr: json['deptAcr'] as String,
+    deptNum: json['deptNum'] as String,
+    credit: json['credit'] as String,
+    status: _$enumDecodeNullable(_$StatusEnumMap, json['status']),
+    id: json['id'] as String,
+    teachers: (json['teachers'] as List)?.map((e) => e as String)?.toList(),
+    section: json['section'] as String,
+    classTimes: (json['classTimes'] as List)
+        ?.map((e) =>
+            e == null ? null : ClassTime.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    objectID: json['objectID'] as String,
+  );
 }
 
 Map<String, dynamic> _$OfferingToJson(Offering instance) {
@@ -46,54 +47,67 @@ Map<String, dynamic> _$OfferingToJson(Offering instance) {
   return val;
 }
 
-T _$enumDecode<T>(Map<T, dynamic> enumValues, dynamic source) {
+T _$enumDecode<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
   if (source == null) {
     throw ArgumentError('A value must be provided. Supported values: '
         '${enumValues.values.join(', ')}');
   }
-  return enumValues.entries
-      .singleWhere((e) => e.value == source,
-          orElse: () => throw ArgumentError(
-              '`$source` is not one of the supported values: '
-              '${enumValues.values.join(', ')}'))
-      .key;
+
+  final value = enumValues.entries
+      .singleWhere((e) => e.value == source, orElse: () => null)
+      ?.key;
+
+  if (value == null && unknownValue == null) {
+    throw ArgumentError('`$source` is not one of the supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+  return value ?? unknownValue;
 }
 
-T _$enumDecodeNullable<T>(Map<T, dynamic> enumValues, dynamic source) {
+T _$enumDecodeNullable<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
   if (source == null) {
     return null;
   }
-  return _$enumDecode<T>(enumValues, source);
+  return _$enumDecode<T>(enumValues, source, unknownValue: unknownValue);
 }
 
-const _$StatusEnumMap = <Status, dynamic>{
+const _$StatusEnumMap = {
   Status.Open: 'Open',
   Status.Closed: 'Closed',
-  Status.Waitlist: 'Waitlist'
+  Status.Waitlist: 'Waitlist',
 };
 
 SearchOffering _$SearchOfferingFromJson(Map<String, dynamic> json) {
   return SearchOffering(
-      credit: json['credit'] as String,
-      deptAcr: json['deptAcr'] as String,
-      deptNum: json['deptNum'] as String,
-      name: json['name'] as String,
-      status: _$enumDecodeNullable(_$StatusEnumMap, json['status']),
-      id: json['id'] as String,
-      teachers: (json['teachers'] as List)?.map((e) => e as String)?.toList(),
-      section: json['section'] as String,
-      classTimes: (json['classTimes'] as List)
-          ?.map((e) =>
-              e == null ? null : ClassTime.fromJson(e as Map<String, dynamic>))
-          ?.toList(),
-      deptNumInt: json['deptNumInt'] as int,
-      sectionInt: json['sectionInt'] as int,
-      range: json['range'] == null
-          ? null
-          : ClassTime.fromJson(json['range'] as Map<String, dynamic>),
-      deptName: json['deptName'] as String,
-      school: json['school'] as String,
-      season: json['season'] as String);
+    credit: json['credit'] as String,
+    deptAcr: json['deptAcr'] as String,
+    deptNum: json['deptNum'] as String,
+    name: json['name'] as String,
+    status: _$enumDecodeNullable(_$StatusEnumMap, json['status']),
+    id: json['id'] as String,
+    teachers: (json['teachers'] as List)?.map((e) => e as String)?.toList(),
+    section: json['section'] as String,
+    classTimes: (json['classTimes'] as List)
+        ?.map((e) =>
+            e == null ? null : ClassTime.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    deptNumInt: json['deptNumInt'] as int,
+    sectionInt: json['sectionInt'] as int,
+    range: json['range'] == null
+        ? null
+        : ClassTime.fromJson(json['range'] as Map<String, dynamic>),
+    deptName: json['deptName'] as String,
+    school: json['school'] as String,
+    season: json['season'] as String,
+  );
 }
 
 Map<String, dynamic> _$SearchOfferingToJson(SearchOffering instance) =>
@@ -112,25 +126,22 @@ Map<String, dynamic> _$SearchOfferingToJson(SearchOffering instance) =>
       'deptName': instance.deptName,
       'range': instance.range,
       'school': instance.school,
-      'season': instance.season
+      'season': instance.season,
     };
 
 ClassTime _$ClassTimeFromJson(Map<String, dynamic> json) {
   return ClassTime(
-      location: json['location'] as String,
-      u: json['u'] as bool,
-      m: json['m'] as bool,
-      t: json['t'] as bool,
-      w: json['w'] as bool,
-      r: json['r'] as bool,
-      f: json['f'] as bool,
-      s: json['s'] as bool,
-      end: json['end'] == null
-          ? null
-          : TimeOfDay.fromTimestamp(json['end'] as int),
-      start: json['start'] == null
-          ? null
-          : TimeOfDay.fromTimestamp(json['start'] as int));
+    location: json['location'] as String,
+    u: json['u'] as bool,
+    m: json['m'] as bool,
+    t: json['t'] as bool,
+    w: json['w'] as bool,
+    r: json['r'] as bool,
+    f: json['f'] as bool,
+    s: json['s'] as bool,
+    end: TimeOfDay.fromTimestamp(json['end'] as int),
+    start: TimeOfDay.fromTimestamp(json['start'] as int),
+  );
 }
 
 Map<String, dynamic> _$ClassTimeToJson(ClassTime instance) => <String, dynamic>{
@@ -142,7 +153,6 @@ Map<String, dynamic> _$ClassTimeToJson(ClassTime instance) => <String, dynamic>{
       'r': instance.r,
       'f': instance.f,
       's': instance.s,
-      'start':
-          instance.start == null ? null : TimeOfDay.toTimestamp(instance.start),
-      'end': instance.end == null ? null : TimeOfDay.toTimestamp(instance.end)
+      'start': TimeOfDay.toTimestamp(instance.start),
+      'end': TimeOfDay.toTimestamp(instance.end),
     };
